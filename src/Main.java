a73d0f9c2a658a7efe0c78b1a1ec323002280f4f
Dr.Abdulagaev <magaram.1995@mail.ru>
2023-07-15 18:18:37 +0300

    strokii ispolneno

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args){
+        //task1();
+        //task2();
+        //task3();
+
+
+        String firstName = "Семён";
+        System.out.println("firstName =" + firstName);
+        String middleName = "Семёнович";
+        System.out.println("middleName =" + middleName);
+        String lastName = "Иванов";
+        System.out.println("lastname =" + lastName);
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+
+        System.out.println(fullName.toUpperCase());
+        System.out.println(fullName.replace("ё", "е"));
 
     }
 }
